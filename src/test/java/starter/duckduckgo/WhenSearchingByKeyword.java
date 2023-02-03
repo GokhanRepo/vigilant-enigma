d415073bfc9e2b9628ee53f808498964b9b53b98
GokhanRepo <113057065+GokhanRepo@users.noreply.github.com>
2023-02-04 00:49:56 +0300

    1. Updated WhenSearchingByKeyword.java by declaring action classes as class fields

--- src/test/java/starter/duckduckgo/WhenSearchingByKeyword.java
+++ src/test/java/starter/duckduckgo/WhenSearchingByKeyword.java
@@ -5,6 +5,7 @@ import net.thucydides.core.annotations.Managed;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @ExtendWith(SerenityJUnit5Extension.class)
@@ -13,8 +14,15 @@ class WhenSearchingByKeyword {
     @Managed(driver = "chrome", options = "headless")
     WebDriver driver;
 
+    NavigateActions navigate;
+    SearchActions search;
+    SearchResultSidebar searchResultSidebar;
+
     @Test
     void theKeywordShouldAppearInTheSearchResults() {
+        navigate.toTheDuckDuckGoSearchPage();
+        search.byKeyword("Cucumber");
+        assertThat(searchResultSidebar.heading()).isEqualTo("Cucumber");
 
     }
 }
